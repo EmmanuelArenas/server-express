beec4b69594f3c213b4101ddd7e3aecb0a2cf0da
EmmanuelArenas <emmanuel.live19@gmail.com>
2022-09-26 21:35:31 -0500

    routers y querys

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 const { infoCursos } = require("./cursos");
 // console.log(infoCursos);
 
+//Routers
+const routerProgramacion = express.Router();
+app.use("/api/cursos/programacion", routerProgramacion);
+
+const routerMatematicas = express.Router();
+app.use("/api/cursos/matematicas", routerMatematicas);
+
 // Routing
 app.get("/", (req, res) => {
   res.send("Servidor Express :D");
@@ -14,12 +21,12 @@ app.get("/api/cursos", (req, res) => {
   res.send(JSON.stringify(infoCursos));
 });
 
-// cursos programacion
-app.get("/api/cursos/programacion", (req, res) => {
+// cursos programacion por URL
+routerProgramacion.get("/", (req, res) => {
   res.send(JSON.stringify(infoCursos.programacion));
 });
 
-app.get("/api/cursos/programacion/:lenguaje", (req, res) => {
+routerProgramacion.get("/:lenguaje", (req, res) => {
   const lenguaje = req.params.lenguaje;
   const resultado = infoCursos.programacion.filter(
     (e) => e.lenguaje === lenguaje
@@ -28,15 +35,37 @@ app.get("/api/cursos/programacion/:lenguaje", (req, res) => {
   if (resultado.length === 0) {
     return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
   }
+
+  // console.log(req.query.ordenar);
+  if (req.query.ordenar === "vistas") {
+    return res.send(
+      JSON.stringify(resultado.sort((a, b) => a.vistas - b.vistas))
+    );
+  }
   res.send(JSON.stringify(resultado));
 });
 
-// cursos matematicas
-app.get("/api/cursos/matematicas", (req, res) => {
+routerProgramacion.get("/:lenguaje/:nivel", (req, res) => {
+  const lenguaje = req.params.lenguaje;
+  const nivel = req.params.nivel;
+  const result = infoCursos.programacion.filter(
+    (e) => e.lenguaje === lenguaje && e.nivel === nivel
+  );
+
+  if (result.length === 0) {
+    return res
+      .status(404)
+      .send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+  }
+  res.send(JSON.stringify(result));
+});
+
+// cursos matematicas por URL
+routerMatematicas.get("/", (req, res) => {
   res.send(JSON.stringify(infoCursos.matematicas));
 });
 
-app.get("/api/cursos/matematicas/:tema", (req, res) => {
+routerMatematicas.get("/:tema", (req, res) => {
   const tema = req.params.tema;
   const resultado = infoCursos.matematicas.filter((e) => e.tema === tema);
 
